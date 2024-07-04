bea81db4073dd481ecda497243bd219731208b67
Hammad.Tahir <mhammadt04@gmail.com>
2024-07-04 11:06:27 +0500

    backend validation is added

--- src/login/dto/create-login.dto.ts
+++ src/login/dto/create-login.dto.ts
@@ -1,11 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
-
+import { IsString, IsNotEmpty, IsOptional } from "class-validator";
 export class CreateLoginDto {
     
 @ApiProperty()
+@IsString()
+@IsNotEmpty()
+@IsOptional()
 username : string;
 
 @ApiProperty()
+@IsString()
+@IsNotEmpty()
+@IsOptional()
 password: string;
 
 }
